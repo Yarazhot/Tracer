ddcdd26ef29654ccf90be5c2ca7c50f8d75ed855
Yarazhot <yarik.ybivaet@mail.ru>
2020-10-02 12:27:12 +0300

    initial version

---
+++ Consol_Lab1/Xml_Ser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consol_Lab1
+{
+    class Xml_Ser
+    {
+    }
+}
